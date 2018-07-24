d85a4c76c4ccdbba2e10beac322499c8bec21e52
Paul Jaehne <paul.jaehne@gmail.com>
2018-07-24 21:41:50 +0200

    Finally some basic database operation is working

--- cocktailApp/screens/RandomCocktail.js
+++ cocktailApp/screens/RandomCocktail.js
@@ -1,24 +1,34 @@
-import Expo, { SQLite } from 'expo';
+import Expo, { SQLite, FileSystem as FS, Asset } from 'expo';
 import React, { Component } from 'react';
 import { StyleSheet, Button, View, Text } from 'react-native';
 import CocktailCard from '../components/CocktailCard';
 
-const db = SQLite.openDatabase('../assets/drinks/drinksDB.db');
+//The database needs to be downloaded first, otherwise a new empty database is created!
+FS.downloadAsync(
+	Asset.fromModule(require('../assets/drinks/drinksDB.db')).uri,
+	`${FS.documentDirectory}SQLite/drinksDB.db`
+);
+const db = SQLite.openDatabase('drinksDB.db');
 
 export default class RandomCocktail extends Component {
 	// define initial variables that are required
 	state = {
 		text: 'This is some default context',
+		number: 0,
 		displayCocktail: false,
 	};
 
+	onError = (tx, error) => console.log(error);
+	onSuccess = (tx, results) => this.setState({ text: results.rows._array[0].name });
+
 	getRandomCocktail() {
-		//get a random cocktail ID "SELECT id FROM cocktails ORDER BY RANDOM() LIMIT 1;
+		//get a random cocktail
 		db.transaction(tx => {
-			tx.executeSQL(
+			tx.executeSql(
 				`SELECT * FROM cocktails ORDER BY RANDOM() LIMIT 1;`, //sql statement
 				[], // parameters
-				(_, { rows: { _array } }) => this.setState({ text: { _array } })
+				this.onSuccess,
+				this.onError
 			);
 		});
 		this.setState({ displayCocktail: true });
@@ -30,7 +40,7 @@ export default class RandomCocktail extends Component {
 			<View>
 				<Button title="New Random Cocktail" onPress={() => this.getRandomCocktail()} />
 				<Text>This is a Random Cocktail:</Text>
-				<Text visible={this.state.displayCocktail}>{this.state.text}</Text>
+				<Text>{this.state.text}</Text>
 			</View>
 		);
 	}
