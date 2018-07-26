2ca9a7b445252a5bbf6f7992cae3b784254e9d2a
Paul Jaehne <paul.jaehne@gmail.com>
2018-07-26 12:18:09 +0200

    local cocktail variable with Promises. They ensure that despite async functions calls, all calls are executed in the correct order

--- cocktailApp/screens/RandomCocktail.js
+++ cocktailApp/screens/RandomCocktail.js
@@ -17,68 +17,89 @@ export default class RandomCocktail extends Component {
 	// define initial variables that are required
 	state = {
 		text: 'This is some default context',
-		displayCocktail: false,
 	};
 
-	// define the standard structure of a cocktail, before it is being filled
-	cocktail = {
-		id: null,
-		name: null,
-		glass: null,
-		type: null,
-		thumbnail: null,
-		instruction: null,
-		ingredients: [],
-	};
-
-	getGeneralCocktailInfo() {
-		onSuccess = (tx, results) => {
-			this.cocktail.id = results.rows._array[0].id;
-			this.cocktail.name = results.rows._array[0].name;
-			this.cocktail.glass = results.rows._array[0].glass;
-			this.cocktail.type = results.rows._array[0].type;
-			this.cocktail.thumbnail = results.rows._array[0].thumbnail;
-			this.cocktail.instruction = results.rows._array[0].instruction;
-		};
-		db.transaction(tx => {
-			tx.executeSql(
-				`SELECT c.id, c.name, c.thumbnail, c.instruction, t.name as type, g.type as glass from (SELECT * from cocktails ORDER BY RANDOM() LIMIT 1) c LEFT JOIN types t ON t.id = c.type_id LEFT JOIN glasses g ON g.id = c.glass_id;`,
-				[],
-				onSuccess,
-				(tx, error) => {
-					console.log(error);
-				}
-			);
+	getGeneralCocktailInfo(cocktail) {
+		return new Promise((resolve, reject) => {
+			console.log('start db transaction in getGeneralCocktailInfo');
+			db.transaction(tx => {
+				tx.executeSql(
+					`SELECT c.id, c.name, c.thumbnail, c.instruction, t.name as type, g.type as glass from (SELECT * from cocktails ORDER BY RANDOM() LIMIT 1) c LEFT JOIN types t ON t.id = c.type_id LEFT JOIN glasses g ON g.id = c.glass_id;`,
+					[],
+					(tx, results) => {
+						console.log('getGeneralCocktailInfo success');
+						cocktail.id = results.rows._array[0].id;
+						cocktail.name = results.rows._array[0].name;
+						cocktail.glass = results.rows._array[0].glass;
+						cocktail.type = results.rows._array[0].type;
+						cocktail.thumbnail = results.rows._array[0].thumbnail;
+						cocktail.instruction = results.rows._array[0].instruction;
+						resolve(cocktail);
+					},
+					(tx, error) => {
+						console.log(error);
+						reject(error);
+					}
+				);
+			});
 		});
 	}
 
-	fillCocktailWithIngredients() {
-		onSuccess = (tx, results) => {
-			results.rows._array.forEach(element => {
-				this.cocktail.ingredients.push({ ingredient: element.ingredient, measure: element.measure });
+	fillCocktailWithIngredients(cocktail) {
+		return new Promise((resolve, reject) => {
+			console.log('start db transaction in fillCocktailWithIngredients');
+			db.transaction(tx => {
+				tx.executeSql(
+					`SELECT i.name as ingredient, r.measure FROM ingredients i JOIN recipes r ON i.id = r.ingredient_id JOIN cocktails c ON c.id = r.cocktail_id WHERE c.id = ?;`,
+					[cocktail.id],
+					(tx, results) => {
+						console.log('fillCocktailwithIngredients success');
+						results.rows._array.forEach(element => {
+							cocktail.ingredients.push({ ingredient: element.ingredient, measure: element.measure });
+						});
+						resolve(cocktail);
+					},
+					(tx, error) => {
+						reject(error);
+					}
+				);
 			});
-		};
-		db.transaction(tx => {
-			tx.executeSql(
-				`SELECT i.name as ingredient, r.measure FROM ingredients i JOIN recipes r ON i.id = r.ingredient_id JOIN cocktails c ON c.id = r.cocktail_id WHERE c.id = ?;`,
-				[this.cocktail.id],
-				onSuccess,
-				(tx, error) => {
-					console.log(error);
-				}
-			);
 		});
 	}
 
 	getRandomCocktail() {
-		this.getGeneralCocktailInfo();
-		//this.fillCocktailWithIngredients();
-		this.setState({ text: JSON.stringify(this.cocktail) });
-		this.setState({ displayCocktail: true });
+		let cocktail = {
+			id: null,
+			name: null,
+			glass: null,
+			type: null,
+			thumbnail: null,
+			instruction: null,
+			ingredients: [],
+		};
+
+		console.log('1');
+		this.getGeneralCocktailInfo(cocktail)
+			.then(cocktail => {
+				console.log('2');
+				this.fillCocktailWithIngredients(cocktail)
+					.then(cocktail => {
+						console.log('3');
+						this.setState({ text: JSON.stringify(cocktail) });
+					})
+					.catch(error => {
+						console.log(error);
+					});
+			})
+			.catch(error => {
+				console.log(error);
+			});
+		console.log('4');
 	}
 
 	// render what is displayed
 	render() {
+		console.log('render');
 		return (
 			<View>
 				<Button title="New Random Cocktail" onPress={() => this.getRandomCocktail()} />
