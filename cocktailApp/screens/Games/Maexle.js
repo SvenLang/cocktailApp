784395bdbd1d419811f88680d7b18db8fe8c954d
langenecker <s.langenecker@laepple.de>
2018-07-31 21:59:58 +0200

    Create the logic for rolling the dices and get the maexle result of the throw

--- cocktailApp/screens/Games/Maexle.js
+++ cocktailApp/screens/Games/Maexle.js
@@ -1,16 +1,40 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Button } from "react-native";
 
 export default class Maexle extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+
+    this.state = {
+      diceOne: undefined,
+      diceTwo: undefined,
+      maexleThrowResult: undefined
+    };
+  }
+
+  rollTheDice() {
+    diceOne = Math.floor(Math.random() * 6) + 1;
+    diceTwo = Math.floor(Math.random() * 6) + 1;
+    if (diceOne < diceTwo) {
+      maexleThrowResult = diceTwo * 10 + diceOne;
+    } else {
+      maexleThrowResult = diceOne * 10 + diceTwo;
+    }
+    console.log(diceOne, diceTwo, maexleThrowResult);
+    this.setState({
+      diceOne: diceOne,
+      diceTwo: diceTwo,
+      maexleThrowResult: maexleThrowResult
+    });
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Text>Maexle</Text>
+        <Text>Würfel 1: {this.state.diceOne}</Text>
+        <Text>Würfel 2: {this.state.diceTwo}</Text>
+        <Text>Maexle Ergebniss: {this.state.maexleThrowResult}</Text>
+        <Button title={"roll the dices"} onPress={() => this.rollTheDice()} />
       </View>
     );
   }
