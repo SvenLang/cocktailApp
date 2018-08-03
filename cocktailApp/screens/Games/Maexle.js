ec3665ac6304feaad53207358cfecfc2e8336a7e
langenecker <s.langenecker@laepple.de>
2018-08-04 00:58:33 +0200

    The game logic is working. Now you can plat Maexle in the cocktail app.

--- cocktailApp/screens/Games/Maexle.js
+++ cocktailApp/screens/Games/Maexle.js
@@ -1,5 +1,15 @@
 import React from "react";
-import { StyleSheet, View, Text, Button, TextInput } from "react-native";
+import { StyleSheet, View, Text, Button, TextInput, Image } from "react-native";
+
+const imgDices = [
+  require("../../assets/images/cubeUnmarked.png"),
+  require("../../assets/images/cubeOne.png"),
+  require("../../assets/images/cubeTwo.png"),
+  require("../../assets/images/cubeThree.png"),
+  require("../../assets/images/cubeFour.png"),
+  require("../../assets/images/cubeFive.png"),
+  require("../../assets/images/cubeSix.png")
+];
 
 export default class Maexle extends React.Component {
   constructor(props) {
@@ -8,32 +18,58 @@ export default class Maexle extends React.Component {
     this.state = {
       diceOne: undefined,
       diceTwo: undefined,
+      doublet: false,
       maexleThrowResult: undefined,
       scoreToBeat: 0,
+      typedDiceOne: undefined,
+      typedDiceTwo: undefined,
       typedScore: undefined,
       saidScoreToNeighbor: undefined,
       diced: false,
       scoreIsSaidToNeihbor: false,
-      realMaexleResultThrowedBefore: undefined
+      realMaexleResultThrowedBefore: undefined,
+      doubletThrowedBefore: false,
+      imgDiceOne: imgDices[0],
+      imgDiceTwo: imgDices[0]
     };
   }
+
+  imgDiceOne = "";
 
   rollTheDice() {
     diceOne = Math.floor(Math.random() * 6) + 1;
     diceTwo = Math.floor(Math.random() * 6) + 1;
     if (diceOne < diceTwo) {
       maexleThrowResult = diceTwo * 10 + diceOne;
+      doublet = false;
+    } else if (diceOne === diceTwo) {
+      maexleThrowResult = diceOne * 10 + diceTwo;
+      doublet = true;
     } else {
       maexleThrowResult = diceOne * 10 + diceTwo;
+      doublet = false;
     }
     console.log(diceOne, diceTwo, maexleThrowResult);
     this.setState({
       diceOne: diceOne,
       diceTwo: diceTwo,
+      doublet: doublet,
       maexleThrowResult: maexleThrowResult,
       realMaexleResultThrowedBefore: maexleThrowResult,
-      diced: true
+      diced: true,
+      imgDiceOne: imgDices[diceOne],
+      imgDiceTwo: imgDices[diceTwo]
     });
+    if (maexleThrowResult === 21) {
+      alert("MAEXLE!!!");
+      this.newGameStart();
+    }
+    if (this.state.scoreToBeat === 66 && maexleThrowResult !== 21) {
+      alert(
+        "You don´t throw 21 and the score to beat was 66, so a new Game starts. You Lost!"
+      );
+      this.newGameStart();
+    }
   }
 
   onChangeTextResultToSay(text) {
@@ -41,13 +77,21 @@ export default class Maexle extends React.Component {
       alert("to much numbers");
       return;
     }
-    diceOne = parseInt(text.slice(0, 1));
-    diceTwo = parseInt(text.slice(1, 2));
-    if (diceOne < diceTwo) {
-      alert("no correct maexle result. Perhaps you mean " + diceTwo + diceOne);
+    typedDiceOne = parseInt(text.slice(0, 1));
+    typedDiceTwo = parseInt(text.slice(1, 2));
+    if (typedDiceOne < typedDiceTwo) {
+      alert(
+        "no correct maexle result. Perhaps you mean " +
+          typedDiceTwo +
+          typedDiceOne
+      );
     } else {
-      typedScore = diceOne * 10 + diceTwo;
-      this.setState({ typedScore: typedScore });
+      typedScore = typedDiceOne * 10 + typedDiceTwo;
+      this.setState({
+        typedScore: typedScore,
+        typedDiceOne: typedDiceOne,
+        typedDiceTwo: typedDiceTwo
+      });
     }
   }
 
@@ -56,34 +100,98 @@ export default class Maexle extends React.Component {
       console.log("1");
       console.log(this.state.maexleThrowResult);
       console.log(this.state.scoreToBeat);
-      if (this.state.maexleThrowResult > this.state.scoreToBeat) {
-        console.log("2");
-        this.setState({
-          saidScoreToNeighbor: this.state.maexleThrowResult,
-          scoreIsSaidToNeihbor: true,
-          diceOne: undefined,
-          diceTwo: undefined,
-          maexleThrowResult: undefined
-        });
-      } else {
-        alert("Your result must beat the score to beat!");
+      //Throwed a doublet
+      if (this.state.doublet) {
+        if (this.state.doubletThrowedBefore) {
+          if (this.state.maexleThrowResult > this.state.scoreToBeat) {
+            this.setState({
+              doubletThrowedBefore: true,
+              saidScoreToNeighbor: this.state.maexleThrowResult,
+              scoreIsSaidToNeihbor: true,
+              maexleThrowResult: undefined,
+              imgDiceOne: imgDices[0],
+              imgDiceTwo: imgDices[0]
+            });
+          } else {
+            alert("Your ressult must beat the score to beat!");
+          }
+        } else {
+          this.setState({
+            doubletThrowedBefore: true,
+            saidScoreToNeighbor: this.state.maexleThrowResult,
+            scoreIsSaidToNeihbor: true,
+            maexleThrowResult: undefined,
+            imgDiceOne: imgDices[0],
+            imgDiceTwo: imgDices[0]
+          });
+        }
+      } //No doublet was throwed
+      else if (!this.state.doublet) {
+        if (this.state.doubletThrowedBefore) {
+          alert("Your result must beat the score to beat!");
+        } else {
+          if (this.state.maexleThrowResult > this.state.scoreToBeat) {
+            console.log("2");
+            this.setState({
+              saidScoreToNeighbor: this.state.maexleThrowResult,
+              scoreIsSaidToNeihbor: true,
+              maexleThrowResult: undefined,
+              imgDiceOne: imgDices[0],
+              imgDiceTwo: imgDices[0]
+            });
+          } else {
+            alert("Your result must beat the score to beat!");
+          }
+        }
       }
     }
   }
 
   sayTypedResultToNeighbor() {
     if (this.state.typedScore !== undefined) {
-      if (this.state.typedScore > this.state.scoreToBeat) {
-        this.setState({
-          saidScoreToNeighbor: this.state.typedScore,
-          scoreIsSaidToNeihbor: true,
-          diceOne: undefined,
-          diceTwo: undefined,
-          maexleThrowResult: undefined,
-          typedScore: ""
-        });
-      } else {
-        alert("Your result must beat the score to beat!");
+      //Doublet was throwed before
+      if (this.state.doubletThrowedBefore) {
+        if (
+          this.state.typedDiceOne === this.state.typedDiceTwo &&
+          this.state.typedScore > this.state.scoreToBeat
+        ) {
+          this.setState({
+            saidScoreToNeighbor: this.state.typedScore,
+            scoreIsSaidToNeihbor: true,
+            doubletThrowedBefore: true,
+            maexleThrowResult: undefined,
+            typedScore: "",
+            imgDiceOne: imgDices[0],
+            imgDiceTwo: imgDices[0]
+          });
+        } else {
+          alert("Your result must beate the score to beat!");
+        }
+      } else if (!this.state.doubletThrowedBefore) {
+        if (this.state.typedDiceOne === this.state.typedDiceTwo) {
+          this.setState({
+            saidScoreToNeighbor: this.state.typedScore,
+            scoreIsSaidToNeihbor: true,
+            doubletThrowedBefore: true,
+            maexleThrowResult: undefined,
+            typedScore: "",
+            imgDiceOne: imgDices[0],
+            imgDiceTwo: imgDices[0]
+          });
+        } else {
+          if (this.state.typedScore > this.state.scoreToBeat) {
+            this.setState({
+              saidScoreToNeighbor: this.state.typedScore,
+              scoreIsSaidToNeihbor: true,
+              maexleThrowResult: undefined,
+              typedScore: "",
+              imgDiceOne: imgDices[0],
+              imgDiceTwo: imgDices[0]
+            });
+          } else {
+            alert("Your result must beat the score to beat!");
+          }
+        }
       }
     }
   }
@@ -96,6 +204,10 @@ export default class Maexle extends React.Component {
     });
   }
   notBelieveTheResult() {
+    this.setState({
+      imgDiceOne: imgDices[this.state.diceOne],
+      imgDiceTwo: imgDices[this.state.diceTwo]
+    });
     if (
       this.state.realMaexleResultThrowedBefore ===
       this.state.saidScoreToNeighbor
@@ -112,13 +224,17 @@ export default class Maexle extends React.Component {
     this.setState({
       diceOne: undefined,
       diceTwo: undefined,
+      doublet: false,
       maexleThrowResult: undefined,
       scoreToBeat: 0,
+      typedDiceOne: undefined,
+      typedDiceTwo: undefined,
       typedScore: undefined,
       saidScoreToNeighbor: undefined,
       diced: false,
       scoreIsSaidToNeihbor: false,
-      realMaexleResultThrowedBefore: undefined
+      realMaexleResultThrowedBefore: undefined,
+      doubletThrowedBefore: false
     });
   }
 
@@ -127,8 +243,10 @@ export default class Maexle extends React.Component {
       <View style={styles.container}>
         <Text>said result: {this.state.saidScoreToNeighbor}</Text>
         <Text>Score to beat: {this.state.scoreToBeat}</Text>
-        <Text>Dice 1: {this.state.diceOne}</Text>
-        <Text>Dice 2: {this.state.diceTwo}</Text>
+        <View style={styles.cubesView}>
+          <Image source={this.state.imgDiceOne} style={styles.cubeImg} />
+          <Image source={this.state.imgDiceTwo} style={styles.cubeImg} />
+        </View>
         <Text>Maexle result: {this.state.maexleThrowResult}</Text>
         <Text>Result to say neighbor:</Text>
         <TextInput
@@ -179,5 +297,14 @@ const styles = StyleSheet.create({
   },
   buttongroup: {
     height: 30
+  },
+  cubesView: {
+    flexDirection: "row",
+    justifyContent: "center"
+  },
+  cubeImg: {
+    height: 100,
+    width: 100,
+    margin: 10
   }
 });
