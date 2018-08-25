428993d3f13c6e20f1913a256f6db4a5b4800789
Paul Jaehne <paul.jaehne@gmail.com>
2018-08-25 23:30:03 +0200

    Added a Button for a new Cocktail. Unfortunately, it is always visible.

--- cocktailApp/screens/CocktailList.js
+++ cocktailApp/screens/CocktailList.js
@@ -1,67 +1,67 @@
-import React from "react";
-import { StyleSheet, View } from "react-native";
-import { getDrinks } from "../assets/drinks/DrinksInterface";
-import BasicCocktailList from "../components/BasicCocktailList";
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { getDrinks } from '../assets/drinks/DrinksInterface';
+import BasicCocktailList from '../components/BasicCocktailList';
 
 //const allDrinks = require("../assets/drinks/allDrinksModified.json");
 
 export default class CocktailList extends React.Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      loading: false,
-      allDrinks: [],
-      searchQuery: "",
-      error: null,
-      showSearchBar: this.props.showSearchBar
-    };
-  }
+		this.state = {
+			loading: false,
+			allDrinks: [],
+			searchQuery: '',
+			error: null,
+			showSearchBar: this.props.showSearchBar,
+		};
+	}
 
-  static navigationOptions = {
-    title: "Cocktails"
-  };
+	static navigationOptions = {
+		title: 'Cocktails',
+	};
 
-  componentDidMount() {
-    this.makeRemoteRequest();
-  }
-  makeRemoteRequest = () => {
-    this.setState({ loading: true });
+	componentDidMount() {
+		this.makeRemoteRequest();
+	}
+	makeRemoteRequest = () => {
+		this.setState({ loading: true });
 
-    getDrinks(30, this.state.searchQuery)
-      .then(drinks => {
-        this.setState({ loading: false, allDrinks: drinks });
-      })
-      .catch(error => {
-        this.setState({ error: error, loading: false });
-      });
-  };
+		getDrinks(30, this.state.searchQuery)
+			.then(drinks => {
+				this.setState({ loading: false, allDrinks: drinks });
+			})
+			.catch(error => {
+				this.setState({ error: error, loading: false });
+			});
+	};
 
-  searchBarTextChanged = query => {
-    this.setState({ searchQuery: query }, () => this.makeRemoteRequest());
-  };
+	searchBarTextChanged = query => {
+		this.setState({ searchQuery: query }, () => this.makeRemoteRequest());
+	};
 
-  render() {
-    return (
-      <View>
-        <BasicCocktailList
-          showSearchBar={true}
-          drinks={this.state.allDrinks}
-          searchBarTextChanged={this.searchBarTextChanged}
-        />
-      </View>
-    );
-  }
+	render() {
+		return (
+			<View>
+				<BasicCocktailList
+					showSearchBar={true}
+					drinks={this.state.allDrinks}
+					searchBarTextChanged={this.searchBarTextChanged}
+				/>
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff"
-  },
-  rating: {
-    position: "absolute",
-    top: 35,
-    right: 55
-  }
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+	},
+	rating: {
+		position: 'absolute',
+		top: 35,
+		right: 55,
+	},
 });
--- cocktailApp/screens/RandomCocktail.js
+++ cocktailApp/screens/RandomCocktail.js
@@ -1,122 +1,122 @@
-import Expo, { SQLite, FileSystem as FS, Asset } from "expo";
-import React, { Component } from "react";
-import { StyleSheet, Button, View, Text } from "react-native";
-import CocktailCard from "../components/CocktailCard";
+import Expo, { SQLite, FileSystem as FS, Asset } from 'expo';
+import React, { Component } from 'react';
+import { StyleSheet, Button, View, Text } from 'react-native';
+import CocktailCard from '../components/CocktailCard';
 
 //database usage example!
 // https://github.com/expo/test-suite/blob/master/tests/SQLite.js
 
 //The database needs to be downloaded first, otherwise a new empty database is created!
 FS.downloadAsync(
-  Asset.fromModule(require("../assets/drinks/drinksDB.db")).uri,
-  `${FS.documentDirectory}SQLite/drinksDB.db`
+	Asset.fromModule(require('../assets/drinks/drinksDB.db')).uri,
+	`${FS.documentDirectory}SQLite/drinksDB.db`
 );
-const db = SQLite.openDatabase("drinksDB.db");
+const db = SQLite.openDatabase('drinksDB.db');
 
 export default class RandomCocktail extends Component {
-  // define initial variables that are required
-  state = {
-    //this needs to be undefined, otherwise the CocktailCard will be loaded instantly!
-    selectedCocktail: undefined,
-    visible: false
-  };
+	// define initial variables that are required
+	state = {
+		//this needs to be undefined, otherwise the CocktailCard will be loaded instantly!
+		selectedCocktail: undefined,
+		visible: false,
+	};
 
-  getGeneralCocktailInfo(cocktail) {
-    return new Promise((resolve, reject) => {
-      db.transaction(tx => {
-        tx.executeSql(
-          `SELECT c.id, c.name, c.rating, c.thumbnail, c.instruction, t.name as type, g.type as glass from (SELECT * from cocktails ORDER BY RANDOM() LIMIT 1) c LEFT JOIN types t ON t.id = c.type_id LEFT JOIN glasses g ON g.id = c.glass_id;`,
-          [],
-          (tx, results) => {
-            cocktail.id = results.rows._array[0].id;
-            cocktail.name = results.rows._array[0].name;
-            cocktail.glass = results.rows._array[0].glass;
-            cocktail.rating = results.rows._array[0].rating;
-            cocktail.category = results.rows._array[0].type;
-            cocktail.drinkThumb = results.rows._array[0].thumbnail;
-            cocktail.instructions = results.rows._array[0].instruction;
-            resolve(cocktail);
-          },
-          (tx, error) => {
-            reject(error);
-          }
-        );
-      });
-    });
-  }
+	getGeneralCocktailInfo(cocktail) {
+		return new Promise((resolve, reject) => {
+			db.transaction(tx => {
+				tx.executeSql(
+					`SELECT c.id, c.name, c.rating, c.thumbnail, c.instruction, t.name as type, g.type as glass from (SELECT * from cocktails ORDER BY RANDOM() LIMIT 1) c LEFT JOIN types t ON t.id = c.type_id LEFT JOIN glasses g ON g.id = c.glass_id;`,
+					[],
+					(tx, results) => {
+						cocktail.id = results.rows._array[0].id;
+						cocktail.name = results.rows._array[0].name;
+						cocktail.glass = results.rows._array[0].glass;
+						cocktail.rating = results.rows._array[0].rating;
+						cocktail.category = results.rows._array[0].type;
+						cocktail.drinkThumb = results.rows._array[0].thumbnail;
+						cocktail.instructions = results.rows._array[0].instruction;
+						resolve(cocktail);
+					},
+					(tx, error) => {
+						reject(error);
+					}
+				);
+			});
+		});
+	}
 
-  fillCocktailWithIngredients(cocktail) {
-    return new Promise((resolve, reject) => {
-      db.transaction(tx => {
-        tx.executeSql(
-          `SELECT i.name as ingredient, r.measure FROM ingredients i JOIN recipes r ON i.id = r.ingredient_id JOIN cocktails c ON c.id = r.cocktail_id WHERE c.id = ?;`,
-          [cocktail.id],
-          (tx, results) => {
-            results.rows._array.forEach(element => {
-              cocktail.ingredients.push({
-                ingredient: element.ingredient,
-                measure: element.measure
-              });
-            });
-            resolve(cocktail);
-          },
-          (tx, error) => {
-            reject(error);
-          }
-        );
-      });
-    });
-  }
+	fillCocktailWithIngredients(cocktail) {
+		return new Promise((resolve, reject) => {
+			db.transaction(tx => {
+				tx.executeSql(
+					`SELECT i.name as ingredient, r.measure FROM ingredients i JOIN recipes r ON i.id = r.ingredient_id JOIN cocktails c ON c.id = r.cocktail_id WHERE c.id = ?;`,
+					[cocktail.id],
+					(tx, results) => {
+						results.rows._array.forEach(element => {
+							cocktail.ingredients.push({
+								ingredient: element.ingredient,
+								measure: element.measure,
+							});
+						});
+						resolve(cocktail);
+					},
+					(tx, error) => {
+						reject(error);
+					}
+				);
+			});
+		});
+	}
 
-  getRandomCocktail() {
-    let cocktail = {
-      id: null,
-      name: null,
-      glass: null,
-      rating: 0,
-      category: null,
-      drinkThumb: null,
-      instructions: null,
-      ingredients: []
-    };
+	getRandomCocktail() {
+		let cocktail = {
+			id: null,
+			name: null,
+			glass: null,
+			rating: 0,
+			category: null,
+			drinkThumb: null,
+			instructions: null,
+			ingredients: [],
+		};
 
-    this.getGeneralCocktailInfo(cocktail)
-      .then(cocktail => {
-        this.fillCocktailWithIngredients(cocktail)
-          .then(cocktail => {
-            console.log(cocktail);
-            this.setState({ visible: true, selectedCocktail: cocktail });
-          })
-          .catch(error => {
-            console.log(error);
-            this.setState({ visible: false });
-          });
-      })
-      .catch(error => {
-        console.log(error);
-        this.setState({ visible: false });
-      });
-  }
+		this.getGeneralCocktailInfo(cocktail)
+			.then(cocktail => {
+				this.fillCocktailWithIngredients(cocktail)
+					.then(cocktail => {
+						console.log(cocktail);
+						this.setState({ visible: true, selectedCocktail: cocktail });
+					})
+					.catch(error => {
+						console.log(error);
+						this.setState({ visible: false });
+					});
+			})
+			.catch(error => {
+				console.log(error);
+				this.setState({ visible: false });
+			});
+	}
 
-  // render what is displayed
-  render() {
-    console.log("render");
-    return (
-      <View>
-        <Button
-          title="New Random Cocktail"
-          onPress={() => this.getRandomCocktail()}
-        />
-        <Text>This is a Random Cocktail:</Text>
-        <Text>{JSON.stringify(this.state.selectedCocktail)}</Text>
-        <CocktailCard
-          visible={this.state.visible}
-          cocktailToShow={this.state.selectedCocktail}
-          onRequestClose={() => this.setState({ visible: false })}
-        />
-      </View>
-    );
-  }
+	// render what is displayed
+	render() {
+		console.log('render');
+		return (
+			<View>
+				<Button title="New Random Cocktail" onPress={() => this.getRandomCocktail()} />
+				<Text>This is a Random Cocktail:</Text>
+				<Text>{JSON.stringify(this.state.selectedCocktail)}</Text>
+				<CocktailCard
+					visible={this.state.visible}
+					cocktailToShow={this.state.selectedCocktail}
+					onRequestClose={() => this.setState({ visible: false })}
+					buttonVisible={true}
+					buttonTitle={'New Random Cocktail'}
+					buttonAction={() => this.getRandomCocktail()}
+				/>
+			</View>
+		);
+	}
 
-  // set the styles
+	// set the styles
 }
