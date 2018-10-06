583e0d9777b544c57f5883d7b610e979e9b6921b
Paul Jaehne <paul.jaehne@gmail.com>
2018-10-06 17:59:52 +0200

    Removed Search Tab from BottomNavigation

--- cocktailApp/navigation/MainTabNavigator.js
+++ cocktailApp/navigation/MainTabNavigator.js
@@ -1,95 +1,75 @@
-import React from "react";
-import { Platform } from "react-native";
-import {
-  createStackNavigator,
-  createBottomTabNavigator
-} from "react-navigation";
+import React from 'react';
+import { Platform } from 'react-native';
+import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
 
-import TabBarIcon from "../components/TabBarIcon";
-import Cocktails from "../screens/Cocktails";
-import SearchScreen from "../screens/SearchScreen";
-import GameScreen from "../screens/GameScreen";
-import NewCocktailScreen from "../screens/NewCocktailScreen";
+import TabBarIcon from '../components/TabBarIcon';
+import Cocktails from '../screens/Cocktails';
+import SearchScreen from '../screens/SearchScreen';
+import GameScreen from '../screens/GameScreen';
+import NewCocktailScreen from '../screens/NewCocktailScreen';
 
 const CocktailsStack = createStackNavigator({
-  Cocktails: Cocktails
+	Cocktails: Cocktails,
 });
 
 CocktailsStack.navigationOptions = {
-  tabBarLabel: "Cocktails",
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={
-        Platform.OS === "ios"
-          ? `ios-information-circle${focused ? "" : "-outline"}`
-          : "glass-tulip"
-      }
-      type="material-community"
-    />
-  )
+	tabBarLabel: 'Cocktails',
+	tabBarIcon: ({ focused }) => (
+		<TabBarIcon
+			focused={focused}
+			name={Platform.OS === 'ios' ? `ios-information-circle${focused ? '' : '-outline'}` : 'glass-tulip'}
+			type="material-community"
+		/>
+	),
 };
 
 const SearchStack = createStackNavigator({
-  Search: SearchScreen
+	Search: SearchScreen,
 });
 
 SearchStack.navigationOptions = {
-  tabBarLabel: "Search",
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={
-        Platform.OS === "ios"
-          ? `ios-information-circle${focused ? "" : "-outline"}`
-          : "search"
-      }
-      type=""
-    />
-  )
+	tabBarLabel: 'Search',
+	tabBarIcon: ({ focused }) => (
+		<TabBarIcon
+			focused={focused}
+			name={Platform.OS === 'ios' ? `ios-information-circle${focused ? '' : '-outline'}` : 'search'}
+			type=""
+		/>
+	),
 };
 
 const NewCocktailStack = createStackNavigator({
-  NewCocktail: NewCocktailScreen
+	NewCocktail: NewCocktailScreen,
 });
 
 NewCocktailStack.navigationOptions = {
-  tabBarLabel: "New Cocktail",
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={
-        Platform.OS === "ios"
-          ? `ios-information-circle${focused ? "" : "-outline"}`
-          : "add"
-      }
-      type=""
-    />
-  )
+	tabBarLabel: 'New Cocktail',
+	tabBarIcon: ({ focused }) => (
+		<TabBarIcon
+			focused={focused}
+			name={Platform.OS === 'ios' ? `ios-information-circle${focused ? '' : '-outline'}` : 'add'}
+			type=""
+		/>
+	),
 };
 
 const GamesStack = createStackNavigator({
-  Games: GameScreen
+	Games: GameScreen,
 });
 
 GamesStack.navigationOptions = {
-  tabBarLabel: "Games",
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={
-        Platform.OS === "ios"
-          ? `ios-information-circle${focused ? "" : "-outline"}`
-          : "md-game-controller-b"
-      }
-      type="ionicon"
-    />
-  )
+	tabBarLabel: 'Games',
+	tabBarIcon: ({ focused }) => (
+		<TabBarIcon
+			focused={focused}
+			name={Platform.OS === 'ios' ? `ios-information-circle${focused ? '' : '-outline'}` : 'md-game-controller-b'}
+			type="ionicon"
+		/>
+	),
 };
 
 export default createBottomTabNavigator({
-  CocktailsStack,
-  SearchStack,
-  NewCocktailStack,
-  GamesStack
+	CocktailsStack,
+	NewCocktailStack,
+	GamesStack,
 });
